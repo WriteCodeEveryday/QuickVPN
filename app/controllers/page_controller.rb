abed2f2e4053a3a7a6f65afc85b2a45ca2ba0df2
Lazaro Herrera <lazherrera@gmail.com>
2017-01-14 09:21:07 -0500

    Setup project for materialize and refactor the pages.

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,7 @@
+class PageController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  root to: 'page#index'
+  get '/about' => 'page#about'
 end
