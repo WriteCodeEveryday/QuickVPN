6cd063e077ff036eaa7d80b32c8c1f879908661e
Rene Alfonso <functionr@gmail.com>
2017-01-14 17:35:28 -0500

    Integrated Nokogiri

--- vpn_usage.rb
+++ vpn_usage.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'mechanize'
+require 'Nokogiri'
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 I_KNOW_THAT_OPENSSL_VERIFY_PEER_EQUALS_VERIFY_NONE_IS_WRONG = nil
@@ -17,15 +18,21 @@ a.get('https://www.privateinternetaccess.com/pages/client-sign-in') do |signin_p
 		form.user = vpnUser
 		form.pass = vpnPassword
 	end.submit
-	puts my_page.uri
+	#puts my_page.uri
 	
 	#Find the reset credential form.
 	my_page = my_page.form_with(:action=>'https://www.privateinternetaccess.com/pages/ccp-gen-x-password') do |regen_form|
 		#Find the value for user name.
 		#Find the value for password.
 	end.submit
-		
-	#Extract new username
-	#Extract new password
+	
+	values = []
+	parsed_page = Nokogiri::HTML(my_page.body)
+	parsed_page.css('form').css('p').css('strong').map do |a| 
+		values.push(a.inner_html)
+	end
+	
+	# Prints the result.
+	#puts values
 	
 end	
